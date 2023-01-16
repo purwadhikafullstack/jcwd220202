cdb1a8d5d01d8317c2038c0f5824260989f9663d
Bryan Karta <karta.bryan@gmail.com>
2023-01-16 20:00:22 +0700

    update route

--- projects/server/src/routes/loginRoute.js
+++ projects/server/src/routes/loginRoute.js
@@ -3,7 +3,7 @@ const loginController = require("../controllers/loginController");
 const { verifyToken } = require("../middlewares/loginMiddleware");
 const router = express.Router();
 
-router.post("/api/login", loginController.loginUser);
-router.get("/api/refreshToken", verifyToken, loginController.keepUserLoggedIn);
+router.post("/login", loginController.loginUser);
+router.get("/refreshToken", verifyToken, loginController.keepUserLoggedIn);
 
 module.exports = router;
