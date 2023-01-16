f63f6060d53c6ca02a31b4e0b4f58e22f57736c2
Wibi Hanif Wibowo <wibihanifwibowo@gmail.com>
2023-01-16 23:39:20 +0700

    updating

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use("/api", express.static(path.join(__dirname, "public")));
+app.use("/api", express.static(path.join(__dirname, ".././public")));
 //#region API ROUTES
 
 // ===========================
